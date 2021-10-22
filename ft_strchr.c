90024293d8bfbc1ebd39d1dd4525c14acfbb50f6
codesshaman <nicksemail@yandex.ru>
2021-10-22 09:40:23 +0300

    atoi, bzero, calloc, isalnum, isalpha, isascii isdigit, isprint, memchr, memcmp, memcpy, memmove, memset, strchr, strlen

--- ft_strchr.c
+++ ft_strchr.c
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include "ft_strlen.c"
-#include "ft_memchr.c"
+// #include <stdio.h>
+// #include "ft_strlen.c"
+// #include "ft_memchr.c"
 
 // Поиск первого вхождения символа в строке,
 // Его ячейка памяти, если найден или
@@ -24,15 +24,15 @@ char		*ft_strchr(const char *s, int ch)
 	return (ft_memchr(s, ch, ft_strlen(s) + 1));
 }
 
-int main(int argc, char *argv[]){
-	if(argc > 0){
-		char str[] = "abcdef";
-		int th = 3;
-		int ch = 100;
-		printf("%d", ft_strchr(str, th));
-		printf("%c", '\n');
-		printf("%d", ft_strchr(str, ch));
-		printf("%c", '\n');
-	}
-	return (0);
-}
+// int main(int argc, char *argv[]){
+// 	if(argc > 0){
+// 		char str[] = "abcdef";
+// 		int th = 3;
+// 		int ch = 100;
+// 		printf("%d", ft_strchr(str, th));
+// 		printf("%c", '\n');
+// 		printf("%d", ft_strchr(str, ch));
+// 		printf("%c", '\n');
+// 	}
+// 	return (0);
+// }
