e78a98e8cc6986fe7e7d70d238d875ddc50f3539
codesshaman <nicksemail@yandex.ru>
2021-10-22 09:50:47 +0300

    atoi, bzero, calloc, isalnum, isalpha, isascii isdigit, isprint, memchr, memcmp, memcpy, memmove, memset, strchr, strdup, strlen

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,8 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h> // Для free
 #include "libft.h"
+// #include <stdio.h>
+// #include "ft_strlen.c"
+// #include "ft_memcpy.c"
 
 // Дублирует строку str, выделяя
 // Под неё память malloc-ом и
@@ -22,12 +24,21 @@ char	*ft_strdup(const char *str)
 	size_t n;
 	char *res;
 	
-	n = ft_strlen(*str) + 1;
+	n = (size_t)(ft_strlen(str) + 1);
 	res = NULL;
 	res = (char *)malloc(n);
 	if (res == NULL)
 		return (0);
-	ft_memcpy(res, *str, n);
+	ft_memcpy(res, str, n);
 	//free (res);
 	return (res);
 }
+
+// int main(int argc, char *argv[]){
+// 	if(argc > 0){
+// 		char str[7] = "abcdefg";
+// 		printf("%s", ft_strdup(str));
+// 		printf("%c", '\n');
+// 	}
+// 	return (0);
+// }
