3011e6e763a7264a69511a8e63cb0e273b57619d
codesshaman <nicksemail@yandex.ru>
2021-10-25 23:42:53 +0300

    part II

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleslee <jleslee@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 23:17:32 by jleslee           #+#    #+#             */
+/*   Updated: 2021/10/25 23:34:39 by jleslee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//******************Part II******************//
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	number;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number = -n;
+	}
+	else
+		number = n;
+	if (number >= 10)
+		ft_putnbr_fd(number / 10, fd);
+	ft_putchar_fd(number % 10 + '0', fd);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleslee <jleslee@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 20:53:38 by jleslee           #+#    #+#             */
+/*   Updated: 2021/10/25 23:32:01 by jleslee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//******************Part II******************//
+
+#include "libft.h"
+// #include <libft.h>
+// #include "ft_strlen.c"
+// #include "ft_strdup.c"
+
+// int     mapi_toupper(int i, char c)
+// {
+//     char buff;
+
+//     buff = c;
+// 	if (i > 96 && i < 123)
+// 		return (i - 32);
+// 	return (i);
+// }
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    int i;
+    char *res;
+
+    i = 0;
+    if (!s || !f)
+        return (NULL);
+    if (!(res = ft_strdup(s)))        
+		return (NULL);
+    while (res[i])
+	{
+		res[i] = f(i, res[i]);
+		i++;
+	}
+    return (res);
+}
+
+// int main(void){
+//     printf("%s", ft_strmapi("Hello, world!", mapi_toupper()));
+//     return (0);
+// }
