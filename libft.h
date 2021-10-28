fed9d4c7f8f2080b92a53f8f10cc6730641c8adc
codesshaman <nicksemail@yandex.ru>
2021-10-28 13:57:39 +0300

    libft header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jleslee <jleslee@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 09:18:51 by jleslee           #+#    #+#             */
-/*   Updated: 2021/10/26 22:06:21 by jleslee          ###   ########.fr       */
+/*   Updated: 2021/10/28 12:44:01 by jleslee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,11 @@ void		ft_bzero(void *b, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 size_t		*ft_strlcpy(char *dest, const char *src, size_t n);
-size_t		*ft_strlcat(char *dest, const char *src, size_t n);
+size_t		ft_strlcat(char *dest, const char *src, size_t n);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
-char		*ft_strchr(const char *s, int ch);
-char		*ft_strrchr(const char *s, int ch);
+char		*ft_strchr(const char *str, int ch);
+char		*ft_strrchr(const char *str, int ch);
 int			ft_strncmp(const char *arr1, const char *arr2, size_t n);
 void		*ft_memchr(const void *b, int c, size_t n);
 int			ft_memcmp(const void *arr1, const void *arr2, size_t n);
